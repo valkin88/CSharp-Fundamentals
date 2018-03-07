c7030fbae5b54b9dea7a8cdd94e6453b20727272
Valentin Tsonev <valiotsonev@gmail.com>
2018-03-07 20:11:30 +0200

    Uploading source code for DateModifier

---
+++ CSharp OOP Basics/Defining Classes - Exercises/DateModifier/DateModifier/DateModifier.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+public class DateModifier
+{
+    private DateTime startDate;
+    private DateTime endDate;
+
+    public DateTime StartDate
+    {
+        get { return this.startDate; }
+        set { this.startDate = value; }
+    }
+
+    public DateTime EndDate
+    {
+        get { return this.endDate; }
+        set { this.endDate = value; }
+    }
+
+    public DateModifier(DateTime startDate, DateTime endDate)
+    {
+        this.StartDate = startDate;
+        this.EndDate = endDate;
+    }
+
+    public double CountInDays()
+    {
+        double days = Math.Abs((this.StartDate - this.EndDate).TotalDays);
+        return days;
+    }
+
+
+}
---
+++ CSharp OOP Basics/Defining Classes - Exercises/DateModifier/DateModifier/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+public class Program
+{
+    static void Main(string[] args)
+    {
+        DateTime startDate = DateTime.ParseExact(Console.ReadLine(), "yyyy MM dd", CultureInfo.InvariantCulture);
+        DateTime endDate = DateTime.ParseExact(Console.ReadLine(), "yyyy MM dd", CultureInfo.InvariantCulture);
+        DateModifier date = new DateModifier(startDate, endDate);
+
+        Console.WriteLine(date.CountInDays());
+    }
+}
