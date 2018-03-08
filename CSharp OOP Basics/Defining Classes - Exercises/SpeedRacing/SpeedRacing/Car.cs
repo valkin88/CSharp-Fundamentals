8800c1e2536929069d75383e037c1b883824d4e8
Valentin Tsonev <valiotsonev@gmail.com>
2018-03-08 17:51:49 +0200

    Upload source code for SpeedRacing

---
+++ CSharp OOP Basics/Defining Classes - Exercises/SpeedRacing/SpeedRacing/Car.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Car
+{
+    private string carModel;
+    private decimal fuelAmount;
+    private decimal fuelConsumptionFor1km;
+    private decimal distanceTraveled;
+
+    private List<Car> cars;
+
+    public List<Car> Cars
+    {
+        get { return cars; }
+        set { cars = value; }
+    }
+
+    public Car()
+    {
+        this.cars = new List<Car>();
+    }
+
+
+    public string CarModel
+    {
+        get { return carModel; }
+        set { carModel = value; }
+    }
+
+    public decimal FuelAmount
+    {
+        get { return fuelAmount; }
+        set { fuelAmount = value; }
+    }
+
+    public decimal FuelConsumptionFor1km
+    {
+        get { return fuelConsumptionFor1km; }
+        set { fuelConsumptionFor1km = value; }
+    }
+
+    public decimal DistanceTraveled
+    {
+        get { return distanceTraveled; }
+        set { distanceTraveled = value; }
+    }
+
+    public void Drive(decimal kmToDrive)
+    {
+        if (this.FuelAmount < kmToDrive * this.FuelConsumptionFor1km)
+        {
+            Console.WriteLine("Insufficient fuel for the drive");
+        }
+        else
+        {
+            this.FuelAmount -= kmToDrive * this.FuelConsumptionFor1km;
+            this.DistanceTraveled += kmToDrive;
+        }
+    }
+}
---
+++ CSharp OOP Basics/Defining Classes - Exercises/SpeedRacing/SpeedRacing/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int numberOfCars = int.Parse(Console.ReadLine());
+        
+        List<Car> cars = new List<Car>();
+        string model = String.Empty;
+        decimal amountOfFuel = 0;
+        decimal fuelConsumption = 0;
+
+        for (int countCars = 0; countCars < numberOfCars; countCars++)
+        {
+            string[] input = Console.ReadLine().Split();
+            Car car = new Car();
+            car.CarModel = input[0];
+            car.FuelAmount = decimal.Parse(input[1]);
+            car.FuelConsumptionFor1km = decimal.Parse(input[2]);
+            car.DistanceTraveled = 0;
+            cars.Add(car);
+
+        }
+
+        var inputLines = Console.ReadLine();
+        decimal kmToDrive = 0;
+        
+        while (inputLines != "End")
+        {
+            var enteringCarsInput = inputLines.Split();
+
+            model = enteringCarsInput[1];
+            kmToDrive = decimal.Parse(enteringCarsInput[2]);
+            cars.Where(c => c.CarModel == model).ToList().ForEach(c => c.Drive(kmToDrive));
+
+
+            inputLines = Console.ReadLine();
+
+        }
+
+        foreach (var c in cars)
+        {
+            Console.WriteLine($"{c.CarModel} {c.FuelAmount:F2} {c.DistanceTraveled}");
+        }
+
+
+    }
+
+}
