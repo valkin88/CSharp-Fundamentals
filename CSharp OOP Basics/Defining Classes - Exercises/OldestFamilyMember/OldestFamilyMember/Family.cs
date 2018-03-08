091e0f534549bddfee85c18691238775ad8b1576
Valentin Tsonev <valiotsonev@gmail.com>
2018-03-08 17:49:22 +0200

    Upload source code for OldestFamilyMember

---
+++ CSharp OOP Basics/Defining Classes - Exercises/OldestFamilyMember/OldestFamilyMember/Family.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+public class Family
+{
+    private List<Person> family = new List<Person>();
+
+    public Family()
+    {
+        this.family = new List<Person>();
+        
+    }
+
+    public void AddMember(Person member)
+    {
+        this.family.Add(member);
+    }
+
+    public Person GetOldestMember()
+    {
+        return this.family.OrderByDescending(a => a.Age).FirstOrDefault();
+    }
+
+
+}
---
+++ CSharp OOP Basics/Defining Classes - Exercises/OldestFamilyMember/OldestFamilyMember/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var linesOfInput = int.Parse(Console.ReadLine());
+        Family family = new Family();
+
+        for (int i = 0; i < linesOfInput; i++)
+        {
+            var input = Console.ReadLine().Split(" ");
+            var person = new Person(input[0], int.Parse(input[1]));
+            family.AddMember(person);
+        }
+
+        var oldestOne = family.GetOldestMember();
+        Console.WriteLine($"{oldestOne.Name} {oldestOne.Age}");
+
+        
+    }
+}
