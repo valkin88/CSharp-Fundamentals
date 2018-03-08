305c566d3813d5349ba1907bbbaffae6b5d16f8f
Valentin Tsonev <valiotsonev@gmail.com>
2018-03-08 17:50:27 +0200

    Upload source code for PokemonTrainer

---
+++ CSharp OOP Basics/Defining Classes - Exercises/PokemonTrainer/PokemonTrainer/Pokemon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Pokemon
+{
+    public string name;
+    public string element;
+    public int health;
+
+    public Pokemon(string name, string element, int health)
+    {
+        this.name = name;
+        this.element = element;
+        this.health = health;
+    }
+}
