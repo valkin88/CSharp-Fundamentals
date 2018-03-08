eded81b12c2520fb953366e9fde4cb4096062f98
Valentin Tsonev <valiotsonev@gmail.com>
2018-03-08 17:49:52 +0200

    Upload source code for OpinionPoll

---
+++ CSharp OOP Basics/Defining Classes - Exercises/OpinionPoll/OpinionPoll/Family.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+public class Family
+{
+    private List<Person> family = new List<Person>();
+
+    public Family()
+    {
+        this.family = new List<Person>();
+
+    }
+
+    public void AddMember(Person member)
+    {
+        this.family.Add(member);
+    }
+
+    public List<Person> GetMembersOlderThan30()
+    {
+        return this.family.Where(x => x.Age > 30).OrderBy(x => x.Name).ToList();
+    }
+
+
+}
---
+++ CSharp OOP Basics/Defining Classes - Exercises/OpinionPoll/OpinionPoll/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var linesOfInput = int.Parse(Console.ReadLine());
+        Family family = new Family();
+
+        for (int i = 0; i < linesOfInput; i++)
+        {
+            var input = Console.ReadLine().Split(" ");
+            var person = new Person(input[0], int.Parse(input[1]));
+            family.AddMember(person);
+        }
+
+        var members = family.GetMembersOlderThan30();
+        foreach (var member in members)
+        {
+            Console.WriteLine($"{member.Name} - {member.Age}");
+        }
+        
+
+
+    }
+}
