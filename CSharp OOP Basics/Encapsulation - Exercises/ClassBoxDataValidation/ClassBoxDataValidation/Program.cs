c1a9e3306d442c33fc67de65e970374f05df5b3c
Valentin Tsonev <valiotsonev@gmail.com>
2018-03-11 14:57:57 +0200

    Upload source code for ClassBoxDataValidation

---
+++ CSharp OOP Basics/Encapsulation - Exercises/ClassBoxDataValidation/ClassBoxDataValidation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            Box box = new Box(length, width, height);
+
+            Console.Write(box.ToString());
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        
+    }
+}
