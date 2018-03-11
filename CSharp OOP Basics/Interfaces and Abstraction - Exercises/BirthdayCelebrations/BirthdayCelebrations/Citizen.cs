15868c7dcb964ed0710dcf3ffee78556f813bdc3
Valentin Tsonev <valiotsonev@gmail.com>
2018-03-11 17:13:06 +0200

    Update

---
+++ CSharp OOP Basics/Interfaces and Abstraction - Exercises/BirthdayCelebrations/BirthdayCelebrations/Citizen.cs
@@ -0,0 +1,27 @@
+﻿public class Citizen : IBirthDate
+{
+    private string name;
+    private string birthDate;
+
+    public Citizen(string name)
+    {
+        this.Name = name;
+    }
+
+    public string Name
+    {
+        get { return name; }
+        private set { name = value; }
+    }
+
+    public virtual string BirthDate
+    {
+        get { return birthDate; }
+        set { birthDate = value; }
+    }
+
+    public override string ToString()
+    {
+        return $"{this.BirthDate}";
+    }
+}
