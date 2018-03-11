4a4b009ac9ee579e35fe62c251ee603af43f92fe
Valentin Tsonev <valiotsonev@gmail.com>
2018-03-11 14:56:51 +0200

    Upload source code for AnimalFarm

---
+++ CSharp OOP Basics/Encapsulation - Exercises/AnimalFarm/AnimalFarm/Models/Validator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public static class Validator
+{
+    public static void ValidateName(string name)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            throw new ArgumentException("Name cannot be empty.");
+        }
+    }
+
+    public static void ValidateAge(int age)
+    {
+        if (age < 0 || age > 15)
+        {
+            throw new ArgumentException("Age should be between 0 and 15.");
+        }
+    }
+}
