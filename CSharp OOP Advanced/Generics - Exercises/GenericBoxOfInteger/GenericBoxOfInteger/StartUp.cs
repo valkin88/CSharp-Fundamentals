7c07aa1b6d3b20518d94cbc3fc568d6919aa885b
Valentin Tsonev <valiotsonev@gmail.com>
2018-03-26 23:26:58 +0300

    Uploading BoxOfIntegers exercise from Generics

---
+++ CSharp OOP Advanced/Generics - Exercises/GenericBoxOfInteger/GenericBoxOfInteger/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var countOfInput = int.Parse(Console.ReadLine());
+
+        for (int count = 0; count < countOfInput; count++)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            Box<int> box = new Box<int>(number);
+
+            Console.WriteLine(box);
+        }
+    }
+}
