c201046f2e72b150799252d612c4877857656170
Valentin Tsonev <valiotsonev@gmail.com>
2018-03-27 22:15:25 +0300

    Upload GenericCountMethodString

---
+++ CSharp OOP Advanced/Generics - Exercises/GenericCountMethodString/GenericCountMethodString/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+class StartUp
+{
+    static void Main(string[] args)
+    {
+        var countOfInput = int.Parse(Console.ReadLine());
+        Box<string> box = new Box<string>();
+
+        for (int count = 0; count < countOfInput; count++)
+        {
+            var value = Console.ReadLine();
+
+            box.Add(value);
+        }
+
+        var valueForComparison = Console.ReadLine();
+        var result = box.CompareTo(valueForComparison);
+        Console.WriteLine(result);
+    }
+}
