7a2fe9383644aa3542b12b6be17a4151b9bd0cfb
Valentin Tsonev <valiotsonev@gmail.com>
2018-03-27 22:13:53 +0300

    Upload GenericBoxOfString

---
+++ CSharp OOP Advanced/Generics - Exercises/GenericBoxOfString/GenericBoxOfString/Box.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Box<T>
+{
+    private T value;
+
+    public Box(T value)
+    {
+        this.Value = value;
+    }
+
+    public T Value
+    {
+        get { return this.value; }
+        set { this.value = value; }
+    }
+
+    public override string ToString()
+    {
+        string result = $"{this.Value.GetType().FullName}: {this.Value}";
+
+        return result;
+    }
+}
---
+++ CSharp OOP Advanced/Generics - Exercises/GenericBoxOfString/GenericBoxOfString/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class StartUp
+{
+    static void Main(string[] args)
+    {
+        var countOfInput = int.Parse(Console.ReadLine());
+
+        for (int count = 0; count < countOfInput; count++)
+        {
+            var value = Console.ReadLine();
+
+            Box<string> box = new Box<string>(value);
+
+            Console.WriteLine(box);
+        }
+    }
+}
