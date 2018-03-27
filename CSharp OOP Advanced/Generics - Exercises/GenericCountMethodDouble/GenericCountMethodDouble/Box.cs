6321e7a633b6d1aaaa5a2e6cbe7910c9d738cfc2
Valentin Tsonev <valiotsonev@gmail.com>
2018-03-27 22:14:44 +0300

    Upload GenericCountMethodDouble

---
+++ CSharp OOP Advanced/Generics - Exercises/GenericCountMethodDouble/GenericCountMethodDouble/Box.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Box<T> where T : IComparable<T>
+{
+    private T value;
+    private List<T> values;
+
+    public Box()
+    {
+        this.values = new List<T>();
+    }
+
+    public T Value
+    {
+        get { return this.value; }
+        set { this.value = value; }
+    }
+
+    public void Add(T value) => this.values.Add(value);
+
+    public int CompareTo(T item)
+    {
+        int countOfBiggerValues = 0;
+
+        foreach (var currentValue in this.values)
+        {
+            var comparison = item.CompareTo(currentValue);
+
+            if (comparison < 0)
+            {
+                countOfBiggerValues++;
+            }
+        }
+
+        return countOfBiggerValues;
+    }
+}
---
+++ CSharp OOP Advanced/Generics - Exercises/GenericCountMethodDouble/GenericCountMethodDouble/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+class StartUp
+{
+    static void Main(string[] args)
+    {
+        var countOfInput = int.Parse(Console.ReadLine());
+        Box<double> box = new Box<double>();
+
+        for (int count = 0; count < countOfInput; count++)
+        {
+            var value = double.Parse(Console.ReadLine());
+
+            box.Add(value);
+        }
+
+        var valueForComparison = double.Parse(Console.ReadLine());
+        var result = box.CompareTo(valueForComparison);
+        Console.WriteLine(result);
+    }
+}
