8ddcf0e71f6b59e657a65643a44693ba14c2d21e
Valentin Tsonev <valiotsonev@gmail.com>
2018-03-28 22:48:37 +0300

    Upload GenericSwapMethodInteger

---
+++ CSharp OOP Advanced/Generics - Exercises/GenericSwapMethodInteger/GenericSwapMethodInteger/Box.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Box<T> : IEnumerable<T>
+{
+    private T value;
+    private List<T> values;
+
+    public Box()
+    {
+        this.values = new List<T>();
+    }
+
+    public T Value
+    {
+        get { return this.value; }
+        set { this.value = value; }
+    }
+
+    public void Swap(int firstIndex, int secondIndex)
+    {
+        T firstValue = this.values[firstIndex];
+        T secondValue = this.values[secondIndex];
+        this.values[firstIndex] = secondValue;
+        this.values[secondIndex] = firstValue;
+    }
+
+    public void Add(T value) => this.values.Add(value);
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        return this.values.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return this.values.GetEnumerator();
+    }
+}
---
+++ CSharp OOP Advanced/Generics - Exercises/GenericSwapMethodInteger/GenericSwapMethodInteger/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+class StartUp
+{
+    static void Main(string[] args)
+    {
+        var countOfInput = int.Parse(Console.ReadLine());
+        Box<int> box = new Box<int>();
+
+        for (int count = 0; count < countOfInput; count++)
+        {
+            var value = int.Parse(Console.ReadLine());
+
+            box.Add(value);
+        }
+
+        var swapComand = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+        var firstIndex = swapComand[0];
+        var secondIndex = swapComand[1];
+
+        box.Swap(firstIndex, secondIndex);
+
+        foreach (var value in box)
+        {
+            Console.WriteLine($"{value.GetType().FullName}: {value}");
+        }
+    }
+}
