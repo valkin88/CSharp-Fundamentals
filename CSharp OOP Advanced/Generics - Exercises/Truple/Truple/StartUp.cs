06816285cd80f363c1785d7c4b4023ea79586b4b
Valentin Tsonev <valiotsonev@gmail.com>
2018-03-29 18:54:11 +0300

    Upload Tuple exercise

---
+++ CSharp OOP Advanced/Generics - Exercises/Truple/Truple/StartUp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class StartUp
+{
+    static void Main(string[] args)
+    {
+        string[] inputArgs = Console.ReadLine().Split();
+
+        string fullName = inputArgs[0] + " " + inputArgs[1];
+        string address = inputArgs[2];
+
+        Tuple<string, string> firstTuple =
+            new Tuple<string, string>(fullName, address);
+
+        inputArgs = Console.ReadLine().Split();
+        string personName = inputArgs[0];
+        int beerAmount = int.Parse(inputArgs[1]);
+
+        Tuple<string, int> secondTuple =
+            new Tuple<string, int>(personName, beerAmount);
+
+        inputArgs = Console.ReadLine().Split();
+        int anInteger = int.Parse(inputArgs[0]);
+        double aDouble = double.Parse(inputArgs[1]);
+
+        Tuple<int, double> thirdTuple =
+            new Tuple<int, double>(anInteger, aDouble);
+
+        Console.WriteLine($"{firstTuple.Item1} -> {firstTuple.Item2}");
+        Console.WriteLine($"{secondTuple.Item1} -> {secondTuple.Item2}");
+        Console.WriteLine($"{thirdTuple.Item1} -> {thirdTuple.Item2}");
+
+    }
+}
