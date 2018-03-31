314f164a04811bf8a1ad47c3f73aa7eb2e09ea51
Valentin Tsonev <valiotsonev@gmail.com>
2018-03-31 14:55:34 +0300

    Upload Froggy exercise

---
+++ CSharp OOP Advanced/Iterators and Comparators - Exercises/Froggy/Froggy/Lake.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+public class Lake : IEnumerable<int>
+{
+    private List<int> listOfStones;
+
+    public Lake(List<int> arrayOfStones)
+    {
+        this.listOfStones = arrayOfStones;
+    }
+
+    public IEnumerator<int> GetEnumerator()
+    {
+
+        for (int index = 1; index <= this.listOfStones.Count; index++)
+        {
+            if (index % 2 != 0)
+                yield return this.listOfStones[index - 1];
+        }
+
+        for (int index = this.listOfStones.Count; index >= 1; index--)
+        {
+            if (index % 2 == 0)
+                yield return this.listOfStones[index - 1];
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return this.GetEnumerator();
+    }
+}
---
+++ CSharp OOP Advanced/Iterators and Comparators - Exercises/Froggy/Froggy/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class StartUp
+{
+    static void Main(string[] args)
+    {
+        List<int> listOfStones = Console.ReadLine().Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+        Lake lakeList = new Lake(listOfStones);
+
+        Console.WriteLine(string.Join(", ", lakeList));
+    }
+}
