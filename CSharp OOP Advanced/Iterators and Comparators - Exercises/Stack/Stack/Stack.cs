11eedb39a2de589da452ac4b8feb554e19939413
Valentin Tsonev <valiotsonev@gmail.com>
2018-03-31 14:54:58 +0300

    Upload Stack exercise

---
+++ CSharp OOP Advanced/Iterators and Comparators - Exercises/Stack/Stack/Stack.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class Stack<T> : IEnumerable<T>
+{
+    private IList<T> stackOfElements;
+
+    public Stack()
+    {
+        this.StackOfElements = new List<T>();
+    }
+
+    public IList<T> StackOfElements
+    {
+        get { return stackOfElements; }
+        set { stackOfElements = value; }
+    }
+
+    public void Push(T[] arrayOfElements)
+    {
+        for (int i = 0; i < arrayOfElements.Length; i++)
+        {
+            this.StackOfElements.Add(arrayOfElements[i]);
+        }
+    }
+
+    public T Pop()
+    {
+        if (this.StackOfElements.Count == 0)
+        {
+            throw new Exception("No elements");
+        }
+
+        int lastIndex = this.StackOfElements.Count - 1;
+        T element = this.StackOfElements[lastIndex];
+        this.StackOfElements.Remove(element);
+
+        return element;
+    }
+
+    public override string ToString()
+    {
+        IList<T> copyOfStack = this.StackOfElements.Reverse().ToList();
+        string result = string.Join(Environment.NewLine, copyOfStack) + 
+                        Environment.NewLine +
+                        string.Join(Environment.NewLine, copyOfStack);
+
+        return result;
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        for (int i = 0; i < this.StackOfElements.Count; i++)
+        {
+            yield return this.StackOfElements[i];
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return this.GetEnumerator();
+    }
+}
---
+++ CSharp OOP Advanced/Iterators and Comparators - Exercises/Stack/Stack/StartUp.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+class StartUp
+{
+    static void Main(string[] args)
+    {
+        Stack<string> stack = new Stack<string>();
+
+        while (true)
+        {
+            string[] inputCommands = Console.ReadLine().Split(new string[] { " ", "," }, StringSplitOptions.RemoveEmptyEntries);
+            string command = inputCommands[0];
+
+            switch (command)
+            {
+                case "Push":
+                    stack.Push(inputCommands.Skip(1).ToArray());
+                    break;
+                case "Pop":
+                    try
+                    {
+                        stack.Pop();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+            if (command == "END")
+            {
+                if (stack.Count() == 0)
+                {
+                    break;
+                }
+                Console.WriteLine(stack);
+                break;
+            }
+        }
+    }
+}
