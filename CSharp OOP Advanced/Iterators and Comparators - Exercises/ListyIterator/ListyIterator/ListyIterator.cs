2527e93b4eb0a4514ecf35b10b6bc7dc644f3996
Valentin Tsonev <valiotsonev@gmail.com>
2018-03-31 14:54:23 +0300

    Upload ListyIterator exercise

---
+++ CSharp OOP Advanced/Iterators and Comparators - Exercises/ListyIterator/ListyIterator/ListyIterator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class ListyIterator<T>
+{
+    private IList<T> listOfItems;
+    private int currentIndex;
+    public ListyIterator(T[] listOfItems)
+    {
+        this.ListOfItems = listOfItems;
+        currentIndex = 0;
+    }
+
+    public IList<T> ListOfItems
+    {
+        get { return listOfItems; }
+        set
+        {
+            listOfItems = value;
+        }
+    }
+
+    public bool Move()
+    {
+        if (currentIndex + 1 <= this.ListOfItems.Count - 1)
+        {
+            currentIndex++;
+            Console.WriteLine("True");
+            return true;
+        }
+        Console.WriteLine("False");
+        return false;
+    }
+
+    public void Print()
+    {
+        if (this.ListOfItems.Count == 0)
+        {
+            throw new Exception("Invalid Operation!");
+        }
+        Console.WriteLine($"{this.ListOfItems[currentIndex]}");
+    }
+
+    public bool HasNext()
+    {
+        if (currentIndex + 1 <= this.ListOfItems.Count - 1)
+        {
+            Console.WriteLine("True");
+            return true;
+        }
+        Console.WriteLine("False");
+        return false;
+    }
+}
---
+++ CSharp OOP Advanced/Iterators and Comparators - Exercises/ListyIterator/ListyIterator/StartUp.cs
@@ -0,0 +1,8 @@
+﻿class StartUp
+{
+    static void Main(string[] args)
+    {
+        CommandIterator commandIterator = new CommandIterator();
+        commandIterator.Start();
+    }
+}
