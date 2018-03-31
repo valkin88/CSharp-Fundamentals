f965eb9f40775576f90d99f44ca6c04b71e3deb9
Valentin Tsonev <valiotsonev@gmail.com>
2018-03-31 14:53:42 +0300

    Upload Collection exercise

---
+++ CSharp OOP Advanced/Iterators and Comparators - Exercises/Collection/Collection/CommandIterator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+
+public class CommandIterator
+{
+    private ListyIterator<string> listyIterator;
+
+    public void Start()
+    {
+        string input = Console.ReadLine();
+        while (input != "END")
+        {
+            var inputLine = input.Split(' ').ToArray();
+
+            string command = inputLine[0];
+            var commands = inputLine.Skip(1).ToArray();
+
+            switch (command)
+            {
+                case "Create":
+                    listyIterator = new ListyIterator<string>(commands);
+                    break;
+                case "Move":
+                    listyIterator.Move();
+                    break;
+                case "Print":
+                    try
+                    {
+                        listyIterator.Print();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    break;
+                case "HasNext":
+                    listyIterator.HasNext();
+                    break;
+                case "PrintAll":
+                    try
+                    {
+                        listyIterator.PrintAll();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+            input = Console.ReadLine();
+        }
+    }
+}
---
+++ CSharp OOP Advanced/Iterators and Comparators - Exercises/Collection/Collection/ListyIterator.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ListyIterator<T> : IEnumerable<T>
+{
+    private IList<T> listOfItems;
+    private int currentIndex;
+    public ListyIterator(T[] listOfItems)
+    {
+        this.ListOfItems = listOfItems;
+        currentIndex = 0;
+    }
+
+    public IList<T> ListOfItems
+    {
+        get { return listOfItems; }
+        set
+        {
+            listOfItems = value;
+        }
+    }
+
+    public bool Move()
+    {
+        if (currentIndex + 1 <= this.ListOfItems.Count - 1)
+        {
+            currentIndex++;
+            Console.WriteLine("True");
+            return true;
+        }
+        Console.WriteLine("False");
+        return false;
+    }
+
+    public void Print()
+    {
+        if (this.ListOfItems.Count == 0)
+        {
+            throw new Exception("Invalid Operation!");
+        }
+        Console.WriteLine($"{this.ListOfItems[currentIndex]}");
+    }
+
+    public bool HasNext()
+    {
+        if (currentIndex + 1 <= this.ListOfItems.Count - 1)
+        {
+            Console.WriteLine("True");
+            return true;
+        }
+        Console.WriteLine("False");
+        return false;
+    }
+
+    public void PrintAll()
+    {
+        if (this.ListOfItems.Count == 0)
+        {
+            throw new Exception("Invalid Operation!");
+        }
+        Console.WriteLine(string.Join(" ", this.listOfItems));
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        for (int i = 0; i < this.listOfItems.Count; i++)
+        {
+            yield return this.listOfItems[i];
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return this.GetEnumerator();
+    }
+}
