4f49bbd79c2d218f8f9bd7aed88f89e31c84339c
Valentin Tsonev <valiotsonev@gmail.com>
2018-04-01 22:41:35 +0300

    Upload ComparingObjects exercise

---
+++ CSharp OOP Advanced/Iterators and Comparators - Exercises/ComparingObjects/ComparingObjects/Person.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+public class Person : IComparable<Person>
+{
+    private string name;
+    private int age;
+    private string town;
+
+    public Person(string name, int age, string town)
+    {
+        this.Name = name;
+        this.Age = age;
+        this.Town = town;
+    }
+
+    public string Town
+    {
+        get { return town; }
+        set { town = value; }
+    }
+
+    public int Age
+    {
+        get { return age; }
+        set { age = value; }
+    }
+
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    public int CompareTo(Person person)
+    {
+        int result = -1;
+
+        if (this.Name == person.Name)
+        {
+            if (this.Age == person.Age)
+            {
+                if (this.Town == person.Town)
+                {
+                    result = 0;
+                }
+            }
+        }
+        
+        return result;
+    }
+}
---
+++ CSharp OOP Advanced/Iterators and Comparators - Exercises/ComparingObjects/ComparingObjects/StartUp.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+class StartUp
+{
+    static void Main(string[] args)
+    {
+        List<Person> people = new List<Person>();
+        string input;
+
+        while ((input = Console.ReadLine()) != "END")
+        {
+            string[] inputData = input.Split(' ');
+
+            string personName = inputData[0];
+            int personAge = int.Parse(inputData[1]);
+            string personTown = inputData[2];
+
+            Person person = new Person(personName, personAge, personTown);
+            people.Add(person);
+        }
+
+        int indexOfPersonToCompare = int.Parse(Console.ReadLine()) - 1;
+        var personToCompare = people[indexOfPersonToCompare];
+        int countOfEqualPeople = 0;
+
+        foreach (var person in people)
+        {
+            int resultOfComparison = personToCompare.CompareTo(person);
+
+            if (resultOfComparison == 0)
+            {
+                countOfEqualPeople++;
+            }
+        }
+
+        if (countOfEqualPeople - 1 == 0)
+        {
+            Console.WriteLine("No matches");
+        }
+        else
+        {
+            int notEqualPeople = people.Count - countOfEqualPeople;
+
+            Console.WriteLine($"{countOfEqualPeople} {notEqualPeople} {people.Count}");
+        }
+        
+    }
+}
