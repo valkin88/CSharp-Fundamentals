10eae3ccdd2e418c10f15d523f2c9a26cfc25e0b
Valentin Tsonev <valiotsonev@gmail.com>
2018-04-03 19:46:25 +0300

    Upload HarvestingFields exercise

---
+++ CSharp OOP Advanced/Reflection and Attributes - Exercises/HarvestingFields/HarvestingFields/HarvestingFieldsTest.cs
@@ -0,0 +1,67 @@
+﻿namespace P01_HarvestingFields
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+
+    public class HarvestingFieldsTest
+    {
+        public static void Main()
+        {
+            var type = typeof(HarvestingFields);
+            FieldInfo[] fieldsNeeded = null;
+            var input = Console.ReadLine();
+            
+            while (input != "HARVEST")
+            {
+                var modificator = input;
+
+                switch (modificator)
+                {
+                    case "private":
+                        fieldsNeeded = type.GetFields(BindingFlags.Instance | BindingFlags.NonPublic).Where(f => f.IsPrivate).ToArray();
+                        PrintFields(fieldsNeeded);
+                        break;
+                    case "protected":
+                        fieldsNeeded = type.GetFields(BindingFlags.Instance | BindingFlags.NonPublic).Where(f => f.IsFamily).ToArray();
+                        PrintFields(fieldsNeeded);
+                        break;
+                    case "public":
+                        fieldsNeeded = type.GetFields();
+                        PrintFields(fieldsNeeded);
+                        break;
+                    case "all":
+                        fieldsNeeded = type.GetFields(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+                        PrintFields(fieldsNeeded);
+                        break;
+                    default:
+                        break;
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+
+        private static void PrintFields(FieldInfo[] fieldsNeeded)
+        {
+            string modifier = String.Empty;
+            foreach (var field in fieldsNeeded)
+            {
+                if (field.IsFamily)
+                {
+                    modifier = "protected";
+                }
+                else if (field.IsPublic)
+                {
+                    modifier = "public";
+                }
+                else
+                {
+                    modifier = "private";
+                }
+
+                Console.WriteLine($"{modifier} {field.FieldType.Name} {field.Name}");
+            }
+        }
+    }
+}
