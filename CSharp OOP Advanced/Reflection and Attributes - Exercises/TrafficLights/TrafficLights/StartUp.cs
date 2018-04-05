9c4372b0987fd8d9f646241f2bd4d9ff831913f7
Valentin Tsonev <valiotsonev@gmail.com>
2018-04-05 14:08:59 +0300

    Upload TrafficLight exercise

---
+++ CSharp OOP Advanced/Reflection and Attributes - Exercises/TrafficLights/TrafficLights/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using TrafficLights;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        var allTraficLights = new List<TrafficLight>();
+
+        var inputSignal = Console.ReadLine().Split();
+        var stateChangeCount = int.Parse(Console.ReadLine());
+
+        foreach (var signal in inputSignal)
+        {
+            var initialColorState = (LightColor)Enum.Parse(typeof(LightColor), signal);
+            allTraficLights.Add(new TrafficLight(initialColorState));
+        }
+
+        for (int i = 0; i < stateChangeCount; i++)
+        {
+            foreach (var trafficLight in allTraficLights)
+            {
+                trafficLight.ChangeState();
+            }
+
+            Console.WriteLine(string.Join(" ", allTraficLights));
+        }
+    }
+}
---
+++ CSharp OOP Advanced/Reflection and Attributes - Exercises/TrafficLights/TrafficLights/TrafficLight.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using TrafficLights;
+
+public class TrafficLight
+{
+    private LightColor colorState;
+
+    public TrafficLight(LightColor colorState)
+    {
+        this.colorState = colorState;
+    }
+
+    public void ChangeState()
+    {
+        this.colorState = (LightColor)(((int)this.colorState + 1)
+            % Enum.GetNames(typeof(LightColor)).Length);
+    }
+
+    public override string ToString()
+    {
+        return this.colorState.ToString();
+    }
+}
