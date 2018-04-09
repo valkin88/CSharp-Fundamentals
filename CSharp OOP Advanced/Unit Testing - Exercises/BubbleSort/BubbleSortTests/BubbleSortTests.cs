4ec1310e123bff308fce0f7d3cd01f053220d525
Valentin Tsonev <valiotsonev@gmail.com>
2018-04-09 18:24:48 +0300

    Upload BubbleSortTests exercise

---
+++ CSharp OOP Advanced/Unit Testing - Exercises/BubbleSort/BubbleSortTests/BubbleSortTests.cs
@@ -0,0 +1,24 @@
+﻿namespace BubbleSortTests
+{
+    using NUnit.Framework;
+    using BubbleSort;
+
+    public class BubbleSortTests
+    {
+        [Test]
+        [TestCase(9, 2, 3, 4, 5, 6, 7, 8, 1)]
+        [TestCase(9, 8, 7, 6, 5, 4, 3, 2, 1)]
+        public void BubbleCanSortNumbers(params int[] numbersToSort)
+        {
+            // Arrange
+            var bubble = new BubbleSort();
+            var sortedNumbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            // Act
+            bubble.Sort(numbersToSort);
+
+            // Assert
+            CollectionAssert.AreEqual(sortedNumbers, numbersToSort);
+        }
+    }
+}
