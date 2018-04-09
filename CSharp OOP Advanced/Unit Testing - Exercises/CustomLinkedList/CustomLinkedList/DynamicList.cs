bed84060c9b4259380fe8aa7290cd251068378e2
Valentin Tsonev <valiotsonev@gmail.com>
2018-04-09 18:25:51 +0300

    Upload CustomLinkedList exercise

---
+++ CSharp OOP Advanced/Unit Testing - Exercises/CustomLinkedList/CustomLinkedList/DynamicList.cs
@@ -0,0 +1,193 @@
+﻿namespace CustomLinkedList
+{
+    using System;
+
+    public class DynamicList<T>
+    {
+        private class ListNode
+        {
+            public ListNode(T element)
+            {
+                this.Element = element;
+                this.NextNode = null;
+            }
+
+            public ListNode(T element, ListNode prevNode)
+            {
+                this.Element = element;
+                prevNode.NextNode = this;
+            }
+
+            public T Element { get; set; }
+
+            public ListNode NextNode { get; set; }
+        }
+
+        private ListNode head;
+        private ListNode tail;
+        private int count;
+
+        public DynamicList()
+        {
+            this.head = null;
+            this.tail = null;
+            this.count = 0;
+        }
+
+        public int Count
+        {
+            get
+            {
+                return this.count;
+            }
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index >= this.count || index < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Invalid index: " + index);
+                }
+
+                ListNode currentNode = this.head;
+                for (int i = 0; i < index; i++)
+                {
+                    currentNode = currentNode.NextNode;
+                }
+
+                return currentNode.Element;
+            }
+
+            set
+            {
+                if (index >= this.count || index < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Invalid index: " + index);
+                }
+
+                ListNode currentNode = this.head;
+                for (int i = 0; i < index; i++)
+                {
+                    currentNode = currentNode.NextNode;
+                }
+
+                currentNode.Element = value;
+            }
+        }
+
+        public void Add(T item)
+        {
+            if (this.head == null)
+            {
+                this.head = new ListNode(item);
+                this.tail = this.head;
+            }
+            else
+            {
+                ListNode newNode = new ListNode(item, this.tail);
+                this.tail = newNode;
+            }
+
+            this.count++;
+        }
+
+        public T RemoveAt(int index)
+        {
+            if (index >= this.count || index < 0)
+            {
+                throw new ArgumentOutOfRangeException("Invalid index: " + index);
+            }
+
+            int currentIndex = 0;
+            ListNode currentNode = this.head;
+            ListNode prevNode = null;
+            while (currentIndex < index)
+            {
+                prevNode = currentNode;
+                currentNode = currentNode.NextNode;
+                currentIndex++;
+            }
+
+            this.RemoveListNode(currentNode, prevNode);
+
+            return currentNode.Element;
+        }
+
+        public int Remove(T item)
+        {
+            int currentIndex = 0;
+            ListNode currentNode = this.head;
+            ListNode prevNode = null;
+
+            while (currentNode != null)
+            {
+                if (object.Equals(currentNode.Element, item))
+                {
+                    break;
+                }
+
+                prevNode = currentNode;
+                currentNode = currentNode.NextNode;
+                currentIndex++;
+            }
+
+            if (currentNode != null)
+            {
+                RemoveListNode(currentNode, prevNode);
+                return currentIndex;
+            }
+
+            return -1;
+        }
+
+        public int IndexOf(T item)
+        {
+            int index = 0;
+            ListNode currentNode = this.head;
+            while (currentNode != null)
+            {
+                if (object.Equals(currentNode.Element, item))
+                {
+                    return index;
+                }
+
+                currentNode = currentNode.NextNode;
+                index++;
+            }
+
+            return -1;
+        }
+
+        public bool Contains(T item)
+        {
+            int index = IndexOf(item);
+            bool found = index != -1;
+            return found;
+        }
+
+        private void RemoveListNode(ListNode node, ListNode prevNode)
+        {
+            this.count--;
+            if (count == 0)
+            {
+                this.head = null;
+                this.tail = null;
+            }
+            else if (prevNode == null)
+            {
+                this.head = node.NextNode;
+            }
+            else
+            {
+                prevNode.NextNode = node.NextNode;
+            }
+
+            if (object.ReferenceEquals(this.tail, node))
+            {
+                this.tail = prevNode;
+            }
+        }
+    }
+}
---
+++ CSharp OOP Advanced/Unit Testing - Exercises/CustomLinkedList/CustomLinkedList/StartUp.cs
@@ -0,0 +1,12 @@
+﻿namespace CustomLinkedList
+{
+    using System;
+
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
