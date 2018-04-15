70638c3a653de233fa5772d2d74de8ed42305e84
Valentin Tsonev <valiotsonev@gmail.com>
2018-04-15 19:08:28 +0300

    Upload Iterator exercise

---
+++ CSharp OOP Advanced/Unit Testing - Exercises/Iterator/Iterator/StartUp.cs
@@ -0,0 +1,53 @@
+﻿namespace Iterator
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+
+    class StartUp
+    {
+        public static void Main()
+        {
+            ExecuteCommands();
+        }
+
+        private static void ExecuteCommands()
+        {
+            var InitializationArgs = Console.ReadLine().Split();
+            var iterator = new ListIterator(InitializationArgs.Skip(1));
+
+            var iteratorMethods = iterator.GetType().GetMethods();
+
+            var command = Console.ReadLine();
+
+            while (command != "END")
+            {
+                try
+                {
+                    var parsedMethod = iteratorMethods
+                        .FirstOrDefault(m => m.Name == command);
+
+                    if (parsedMethod == null)
+                    {
+                        Console.WriteLine($"This option {command} does not exists");
+                    }
+
+                    Console.WriteLine(parsedMethod.Invoke(iterator, new object[] { }));
+                }
+                catch (TargetInvocationException tie)
+                {
+                    if (tie.InnerException is InvalidOperationException)
+                    {
+                        Console.WriteLine(tie.InnerException.Message);
+                    }
+                }
+                catch (ArgumentNullException ane)
+                {
+                    Console.WriteLine(ane.Message);
+                }
+
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
